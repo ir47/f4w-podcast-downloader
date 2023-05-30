eefa62ec7819b4da0696eb443fca9e5b7835caaa
Ian Ross <ir47@st-andrews.ac.uk>
2023-05-30 20:29:25 +0100

    Created small POC for the downloader

--- podcastDownloader/runner.py
+++ podcastDownloader/runner.py
@@ -1,5 +1,18 @@
+import requests
+from util import getHTTPParameters
+
 def main():
     print('Running Downloader')
+
+    result = requests.get(url='https://media001.f4wonline.com/dmdocuments/052723wol.mp3', headers=getHTTPParameters())
+
+    print('response: ', result.status_code)
+
+    if result.status_code == 200:
+        print('managed to get podcast!')
+        with open('podcast.mp3', 'wb') as f:
+            f.write(result.content)
+
 
 
 if __name__ == "__main__":
