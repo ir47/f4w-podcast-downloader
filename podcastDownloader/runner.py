43ac90414c7e3141bee07b610a5436f03e9e957a
Ian Ross <ir47@st-andrews.ac.uk>
2024-01-23 21:16:01 +0000

    Added yearly and monthly folder generation

--- podcastDownloader/runner.py
+++ podcastDownloader/runner.py
@@ -17,6 +17,10 @@ def main():
     parser.add_argument('-e', '--end', required=False, type=str, default=None, help='End value of podcasts to download')
     parser.add_argument('-i', '--increments', required=False, type=int, default=0,
                         help='Interval range between podcast episodes')
+    parser.add_argument('-m', '--monthlyFolder', required=False, type=bool, default=True,
+                        help='Create new folder for each month')
+    parser.add_argument('-y', '--yearlyFolder', required=False, type=bool, default=True,
+                        help='Create new folder for each year')
     # TODO Make this only one of 2 values possibly prefix true or false
     parser.add_argument('-f', '--format', required=False, type=str, default='prefix',
                         help='Should the episode indicator be before or after the episode name')
@@ -47,20 +51,33 @@ def main():
     show_end_date = args.get("end")
     show_interval = args.get("interval")
 
-    podcast_downloader(show_name, show_start_date, show_end_date, download_path, user_cookies)
+    monthly_folder = args.get("monthlyFolder")
+    yearly_folder = args.get("yearlyFolder")
+
+    podcast_downloader(show_name, show_start_date, show_end_date, download_path, monthly_folder, yearly_folder,
+                       user_cookies)
 
 
-def podcast_downloader(show_name, show_start_date, show_end_date, download_path, user_cookie, date_format='%m%d%y'):
+def podcast_downloader(show_name, show_start_date, show_end_date, download_path, monthly_folder, yearly_folder,
+                       user_cookie, date_format='%m%d%y'):
     start_date = datetime.strptime(show_start_date, date_format)
     end_date = datetime.strptime(show_end_date, date_format)
 
     delta = timedelta(days=1)
 
+    if yearly_folder:
+        download_path += start_date.strftime('%Y') + '/'
+
+    if monthly_folder:
+        download_path += start_date.strftime('%B') + '/'
+
+    print('Download Path: ', download_path)
+
     if download_path is None or not generate_download_directories(download_path):
         download_path = default_download_path()
 
     while start_date <= end_date:
-        print(start_date, end="\n")
+        print('Podcast Date: ', start_date)
 
         podcast_url = create_download_url(show_name, show_start_date)
         print('Podcast URL: ', podcast_url)
@@ -68,7 +85,21 @@ def podcast_downloader(show_name, show_start_date, show_end_date, download_path,
 
         download_podcast_data(podcast_url, podcast_file_name, user_cookie)
 
+        previous_date = start_date
+
         start_date += delta
+
+        if yearly_folder and previous_date.year != start_date.year:
+            new_download_path = download_path.split(previous_date.strftime('%Y'))[0]
+
+            download_path = new_download_path + start_date.strftime('%Y') + '/'
+
+        if monthly_folder and previous_date.month != start_date.month:
+            new_download_path = download_path.split(previous_date.strftime('%B'))[0]
+
+            download_path = new_download_path + start_date.strftime('%B') + '/'
+
+        generate_download_directories(download_path)
         show_start_date = start_date.strftime(date_format)
 
 
